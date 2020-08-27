2b487092b085f564ae4ddd767f7c1d8a5aea4a39
sugeedarou <linus.schilpp@gmail.com>
2020-08-27 16:02:56 +0200

    fix invisible bar bug

--- content/content.js
+++ content/content.js
@@ -1,7 +1,7 @@
 chrome.storage.local.get('bookmarks', (result) => {
 	if (typeof result.bookmarks != 'undefined') {
 		createSidebar(result.bookmarks)
-		listenToMouseEvents()
+		listenToEvents()
 	}
 })
 
@@ -10,7 +10,7 @@ let bbar = null
 function createSidebar(bookmarks) {
 	bbar = document.createElement('div')
 	const div = document.createElement('div')
-	bbar.className = 'bbar'
+	bbar.className = 'bbar hiddenBBB'
 	for (let bm of bookmarks) {
 		const a = document.createElement('a')
 		const img = document.createElement('img')
@@ -21,15 +21,15 @@ function createSidebar(bookmarks) {
 		bbar.appendChild(div)
 	}
 
-	document.body.appendChild(bbar)
+	document.documentElement.insertAdjacentElement('afterbegin', bbar)
 }
 
-function listenToMouseEvents() {
+function listenToEvents() {
 	window.addEventListener('mousemove', (e) => {
 		if (e.path.includes(bbar)) {
-			bbar.classList.add('visible')
+			bbar.classList.remove('hiddenBBB')
 		} else {
-			bbar.classList.remove('visible')
+			bbar.classList.add('hiddenBBB')
 		}
 	})
 }
