ef8ac1e2b5cc8e4961fe702ae0ca31c0f8c9383d
sugeedarou <linus.schilpp@gmail.com>
2020-08-27 16:59:15 +0200

    add random images to new tabs

--- tab/tab.js
+++ tab/tab.js
@@ -1,11 +1,12 @@
 chrome.storage.local.get('bookmarks', (result) => {
     if (typeof result.bookmarks != 'undefined') {
+        loadBackgroundImage()
         createList(result.bookmarks)
     }
 })
 
 function createList(bookmarks) {
-    const bbar = document.createElement('div')
+    const bbar = document.getElementById('blist')
     const div = document.createElement('div')
     bbar.className = 'blist'
     for (let bm of bookmarks) {
@@ -19,4 +20,14 @@ function createList(bookmarks) {
     }
 
     document.body.appendChild(bbar)
+}
+
+function loadBackgroundImage() {
+    const bg = document.getElementById('background')
+    const img = document.createElement('img')
+    img.src = `https://source.unsplash.com/random/${window.innerWidth}x${window.innerHeight}`
+    img.addEventListener('load', () => {
+        img.classList.add('loaded')
+    })
+    bg.appendChild(img)
 }
